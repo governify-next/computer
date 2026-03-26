28089f42563761ad39669c8c5eb6969f2ac1761f
Javier Fernández Castillo <100673872+JaviFdez7@users.noreply.github.com>
2026-03-26 01:32:22 +0100

    feat: update environment configuration for reporter service

--- src/config/bootConfig.ts
+++ src/config/bootConfig.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: envPath });
 
 export const bootEnv = {
     GOV_LOG_LEVEL: process.env.GOV_LOG_LEVEL || 'INFO',
-    GOV_SERVICE_NAME: process.env.GOV_SERVICE_NAME || 'UnknownService',
-    PORT: process.env.PORT || '3000',
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mern_example',
+    GOV_SERVICE_NAME: process.env.GOV_SERVICE_NAME || 'reporter',
+    PORT: process.env.PORT || '5901',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/governify',
 };
