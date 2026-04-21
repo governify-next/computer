3ff4d78991aabf31353d8e509124ef12ec69bcf9
Javier Fernández Castillo <100673872+JaviFdez7@users.noreply.github.com>
2026-04-22 01:11:49 +0200

    feat: implement JWT authentication middleware and update swagger

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import { metricRoutes } from './routes/metric.routes.js';
 import { eventRoutes } from './routes/event.routes.js';
 import { healthRoutes } from './routes/health.routes.js';
+import { isAuthenticated } from './middlewares/authentication.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
@@ -19,6 +20,7 @@ const swaggerDocument = YAML.load(swaggerPath);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(healthRoutes);
+app.use('/api/v1', isAuthenticated);
 app.use('/api/v1', eventRoutes);
 app.use('/api/v1', metricRoutes);
 app.use(errorHandler);
